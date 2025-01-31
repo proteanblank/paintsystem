c1e67fd2a07837f7c563a0d16b19bcfb557944fb
natapol2547 <robloxplay41@gmail.com>
2025-02-01 01:38:04 +0700

    Update version number

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ bl_info = {
     "author": "Tawan Sunflower, @blastframe",
     "description": "",
     "blender": (4, 1, 0),
-    "version": (1, 1, 8),
+    "version": (1, 1, 9),
     "location": "View3D > Sidebar > Paint System",
     "warning": "",
     "category": "Node",
