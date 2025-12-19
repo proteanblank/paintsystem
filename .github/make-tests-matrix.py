98c324134bbf96431c070311f086edf634f13942
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2025-12-19 15:16:02 +0700

    change: more update to Test Matrix

--- .github/make-tests-matrix.py
+++ .github/make-tests-matrix.py
@@ -40,6 +40,18 @@ jobs = [
         "sha": "released",
         "download_url": "https://download.blender.org/release/Blender4.2/blender-4.2.13-linux-x64.tar.xz",
     },
+    {
+        "version": "4.3.2",
+        "version_x_y": "4.3",
+        "sha": "released",
+        "download_url": "https://download.blender.org/release/Blender4.3/blender-4.3.2-linux-x64.tar.xz",
+    },
+    {
+        "version": "4.4.3",
+        "version_x_y": "4.4",
+        "sha": "released",
+        "download_url": "https://download.blender.org/release/Blender4.4/blender-4.4.3-linux-x64.tar.xz",
+    },
     {  # LTS
         "version": "4.5.2",
         "version_x_y": "4.5",
@@ -57,26 +69,26 @@ jobs = [
 ]
 
 
-def get_daily_builds(jobs: list):
-    resp = request.urlopen("https://builder.blender.org/download/daily/")
-    page = resp.read().decode("utf-8")
-    releases = re.findall(
-        r"(https://cdn.builder.blender.org/download/daily/blender-(((?:4|5)\.\d)\.\d-\w+)\+\S{1,6}\.(\S{12})-linux\.x86_64-release\.tar\.xz)",
-        page,
-    )
-    for release in releases:
-        new_job = {
-            "version": release[1],
-            "version_x_y": release[2],
-            "download_url": release[0],
-            "sha": release[3],
-        }
-        if new_job["version"].removesuffix("-stable") not in [
-            job["version"] for job in jobs
-        ]:
-            jobs.append(new_job)
+# def get_daily_builds(jobs: list):
+#     resp = request.urlopen("https://builder.blender.org/download/daily/")
+#     page = resp.read().decode("utf-8")
+#     releases = re.findall(
+#         r"(https://cdn.builder.blender.org/download/daily/blender-(((?:4|5)\.\d)\.\d-\w+)\+\S{1,6}\.(\S{12})-linux\.x86_64-release\.tar\.xz)",
+#         page,
+#     )
+#     for release in releases:
+#         new_job = {
+#             "version": release[1],
+#             "version_x_y": release[2],
+#             "download_url": release[0],
+#             "sha": release[3],
+#         }
+#         if new_job["version"].removesuffix("-stable") not in [
+#             job["version"] for job in jobs
+#         ]:
+#             jobs.append(new_job)
 
 
-get_daily_builds(jobs)
+# get_daily_builds(jobs)
 matrix = {"include": jobs}
 print(f"matrix={matrix}")
