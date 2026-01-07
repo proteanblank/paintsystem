aca4acbf4a6b0e11a83b87c9d21b2c1967145eae
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-07 22:24:20 +0700

    fix: attribute error when getting unified print settings

--- utils/unified_brushes.py
+++ utils/unified_brushes.py
@@ -7,8 +7,10 @@ def get_unified_settings(context: bpy.types.Context, unified_name: str):
     tool_settings = UnifiedPaintPanel.paint_settings(context)
     if hasattr(context.tool_settings, "unified_paint_settings"):
         ups = context.tool_settings.unified_paint_settings
-    else:
+    elif hasattr(tool_settings, "unified_paint_settings"):
         ups = tool_settings.unified_paint_settings
+    else:
+        return None
         
     brush = tool_settings.brush
     prop_owner = brush
