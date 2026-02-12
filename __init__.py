a4b7947a967893273bb6df0394c5bdaedba399bc
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-02-13 03:08:02 +0700

    bump version number

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "author": "Tawan Sunflower, @blastframe",
     "description": "",
     "blender": (4, 2, 0),
-    "version": (2, 1, 7),
+    "version": (2, 1, 8),
     "location": "View3D > Sidebar > Paint System",
     "warning": "",
     "category": "Paint",
